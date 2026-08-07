43cd54398df033c7ffc8c18319eb469b6761d43f
Vu Lan Nguyen <vulan1999@gmail.com>
2026-08-07 11:30:28 +0700

    finalize

--- server.py
+++ server.py
@@ -24,7 +24,13 @@ def emotion_analyzer():
     sadness = emotion_result["sadness"]
     dominant_emotion = emotion_result["dominant_emotion"]
 
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    if dominant_emotion is None:
+        return "Invalid text! Please try again"
+
+    response_str = f"""For the given statement, the system response is
+    'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}.
+    The dominant emotion is <strong>{dominant_emotion}</strong>."""
+    return response_str
 
 
 @app.route("/")
